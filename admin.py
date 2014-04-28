b00e715e975bc20650b94fd6cfeeb5d0e0d02c03
Raymond Wu <virus.warnning@gmail.com>
2014-04-28 18:24:39 +0800

    refs #8, 變更版型路徑 * 版型路徑改用預設值 views, 簡化設計

---
+++ admin.py
@@ -0,0 +1,15 @@
+# -.- encoding: utf-8 -.-
+#
+# 後台管理
+#
+
+from bottle import route
+from bottle import jinja2_view as view
+from bottle import jinja2_template as template
+from bottle import TEMPLATE_PATH
+
+@route('/admin/info')
+@view('admin/info.html')
+def admin_info():
+	print(TEMPLATE_PATH)
+	return {'name': 'admin_info()'}
--- main.py
+++ main.py
@@ -3,11 +3,8 @@
 
 from bottle import route, run, template, TEMPLATE_PATH
 
-#from jinja2 import Environment, PackageLoader
-#env = Environment(loader=PackageLoader('hello', 'templates'))
-TEMPLATE_PATH.append("./templates")
-
 # 載入所有的 request 處理程式
+import admin
 import hello
 
 run(host='localhost', port=8080)
